dc9b6833595bbd9a6d2901c473639aef374d93c5
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-03-18 14:31:08 +0100

    Playwright installation

---
+++ e2e/Header.spec.ts
@@ -0,0 +1,15 @@
+import { test, expect } from "@playwright/test";
+
+test("click on language selector change language", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
+
+test("click on nav items scroll to anchor", async ({ page }) => {
+  await page.goto("https://playwright.dev/");
+
+  // Expect a title "to contain" a substring.
+  await expect(page).toHaveTitle(/Playwright/);
+});
--- vite.config.ts
+++ vite.config.ts
@@ -7,5 +7,6 @@ export default defineConfig({
   test: {
     environment: "jsdom",
     setupFiles: ["./vitestGlobalSetup.js"],
+    exclude: ["**/e2e/**"],
   },
 });
