8fff9f32c02c31480b2459f6e9102c45e3e83426
Benjamin Lesne <benjamin.lesne@outlook.fr>
2023-12-06 20:06:41 +0100

    added plateforme, updated jegwell description

--- src/components/MyProjects/utils/myProjectsData.ts
+++ src/components/MyProjects/utils/myProjectsData.ts
@@ -1,22 +1,23 @@
 import jegwellPath from "../../../assets/website-screenshots/Jegwell.png";
-import ohmyfoodPath from "../../../assets/website-screenshots/ohmyfood.png";
+import plateforme from "../../../assets/website-screenshots/plateforme-focale.png";
 export const myProjectsData = [
   {
-    name: "Jegwell",
-    websiteUrl: "https://jegwell.fr",
-    sourceCodeUrl: "https://github.com/BenjaminLesne/Jegwell",
+    name: "Plateforme Focale",
+    websiteUrl: "https://focale-stg1-fe.osc-fr1.scalingo.io/",
+    sourceCodeUrl:
+      "https://www.reddit.com/media?url=https%3A%2F%2Fpreview.redd.it%2F7ozal346p6kz.png%3Fwidth%3D960%26crop%3Dsmart%26auto%3Dwebp%26s%3Dd2e91130ac406c1d6b0a4a1a7170fe5a5fc8d5af",
     colorMask: "75, 131, 130",
+    imagePath: plateforme,
+    descriptionKey: "plateforme.description",
+    altKey: "plateforme.alt",
+  },
+  {
+    name: "Jegwell",
+    websiteUrl: "https://jegwell.vercel.app/",
+    sourceCodeUrl: "https://github.com/BenjaminLesne/Jegwell",
+    colorMask: "157, 83, 83",
     imagePath: jegwellPath,
     descriptionKey: "jegwell.description",
     altKey: "jegwell.alt",
-  },
-  {
-    name: "ohmyfood",
-    websiteUrl: "https://benjaminlesne.github.io/benjaminLesne_3_03102021/",
-    sourceCodeUrl: "https://github.com/BenjaminLesne/benjaminLesne_3_03102021/",
-    colorMask: "157, 83, 83",
-    imagePath: ohmyfoodPath,
-    descriptionKey: "ohmyfood.description",
-    altKey: "ohmyfood.alt",
   },
 ];
