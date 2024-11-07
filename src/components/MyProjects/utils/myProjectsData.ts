cf3c4df468251ec63ac4ee7e332f8c4cd07734b2
Benjamin Lesne <benjamin.lesne@outlook.fr>
2024-11-07 21:29:24 +0100

    added scenario explorer

--- src/components/MyProjects/utils/myProjectsData.ts
+++ src/components/MyProjects/utils/myProjectsData.ts
@@ -1,23 +1,32 @@
-import jegwellPath from "../../../assets/website-screenshots/Jegwell.png";
-import plateforme from "../../../assets/website-screenshots/plateforme-focale.png";
+import scenarioExplorerUrl from "../../../assets/website-screenshots/scenarioExplorer.png";
 import { NOT_OPEN_SOURCE_URL } from "../../../utils/constants";
+
 export const myProjectsData = [
+  // {
+  //   name: "Plateforme Focale",
+  //   websiteUrl: "https://focale-stg1-fe.osc-fr1.scalingo.io/",
+  //   sourceCodeUrl: NOT_OPEN_SOURCE_URL,
+  //   colorMask: "75, 131, 130",
+  //   imagePath: plateforme,
+  //   descriptionKey: "plateforme.description",
+  //   altKey: "plateforme.alt",
+  // },
+  // {
+  //   name: "Jegwell",
+  //   websiteUrl: "https://jegwell.vercel.app/",
+  //   sourceCodeUrl: "https://github.com/BenjaminLesne/Jegwell",
+  //   colorMask: "157, 83, 83",
+  //   imagePath: jegwellPath,
+  //   descriptionKey: "jegwell.description",
+  //   altKey: "jegwell.alt",
+  // },
   {
-    name: "Plateforme Focale",
-    websiteUrl: "https://focale-stg1-fe.osc-fr1.scalingo.io/",
+    name: "Scenario explorer",
+    websiteUrl: "https://ae-scenario-explorer.cloud.set.kuleuven.be",
     sourceCodeUrl: NOT_OPEN_SOURCE_URL,
-    colorMask: "75, 131, 130",
-    imagePath: plateforme,
-    descriptionKey: "plateforme.description",
-    altKey: "plateforme.alt",
-  },
-  {
-    name: "Jegwell",
-    websiteUrl: "https://jegwell.vercel.app/",
-    sourceCodeUrl: "https://github.com/BenjaminLesne/Jegwell",
     colorMask: "157, 83, 83",
-    imagePath: jegwellPath,
-    descriptionKey: "jegwell.description",
-    altKey: "jegwell.alt",
+    imagePath: scenarioExplorerUrl,
+    descriptionKey: "scenarioExplorer.description",
+    altKey: "scenarioExplorer.alt",
   },
 ];
